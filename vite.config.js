181bdc637f6e20c9b3f194e7540370d9b85ec9e8
Ukyo1121 <1755379549@qq.com>
2026-04-03 23:26:33 +0800

    fix(vite.config.js):修复Electron 的连接问题

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,10 @@ import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
 export default defineConfig({
     root: "src",
-    plugins: [vue()]
+    plugins: [vue()],
+    server: {
+        host: '127.0.0.1', // 强制 Vite 监听 IPv4 地址，解决与 Electron 的连接问题
+        port: 5173,        // 固定端口号
+        strictPort: true   // 如果 5173 被占用直接报错，防止自动切换到 5174 导致 Electron 找不到
+    }
 });
